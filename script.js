360e261c93797bd32f3cbfa76c7e7cdeec299d85
greg-Da <gregoiredalmasso@gmail.com>
2020-11-15 16:29:02 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var Planet = ["Mars", "Earth", "Mercury", "Pluto", "Venus"];
+
+
+var x = 0;
+while(Planet[x]){
+    console.log(Planet[x]);
+    x++;
+}
+
+var hello = function(){
+    return 'Hello world!'
+}
+
+console.log(hello());
